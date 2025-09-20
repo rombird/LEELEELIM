4627a220029062762e21fb8a94d0e6a47e6b7e9d
inno-inho <zergling2565@gmail.com>
2025-09-21 05:38:13 +0900

    Delete Fashionnet directory

--- Fashionnet/terms_of_service_page/terms_of_service.js
+++
@@ -1,8 +0,0 @@
-// document.querySelectorAll(".step .on em").forEach(btn => {
-//     btn.addEventListener("click", e =>{
-//         document.querySelectorAll(".span .on em").forEach(el => {
-//             el.classList.remove("active")
-//         });
-//     e.target.classList.add("active");
-//     });
-// });
