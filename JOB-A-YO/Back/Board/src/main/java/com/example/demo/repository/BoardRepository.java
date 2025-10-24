4f0ba0aeb18e7b3fcdac6139a76fba451402d558
rombird <romsae5510@gmail.com>
2025-10-24 14:13:45 +0900

    jobayo

--- JOB-A-YO/Back/Board/src/main/java/com/example/demo/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Entity 클래스만 받음
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}
